178d89727c2cde401dc64217af895085180f1fce
Gabriel de Carvalho <gabriel.carvalho@softplan.com.br>
2021-02-24 22:15:06 -0300

    Add CompletedChallenges component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import {CompletedChallenges} from "../components/CompletedChallenges";
 import {ExperienceBar} from "../components/ExperienceBar";
 import {Profile} from "../components/Profile";
 
 import styles from '../styles/pages/Home.module.css'
+
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
       <section>
         <div>
           <Profile/>
+          <CompletedChallenges/>
         </div>
         <div>
 
